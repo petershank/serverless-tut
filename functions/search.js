36ee51f96dc3e6e5343fdf696192a67f80907ec0
Peter Shank <peter@shank.com>
2021-12-21 20:36:40 -0800

    create functions/search.js

---
+++ functions/search.js
@@ -0,0 +1,8 @@
+const qs = require("querystring");
+exports.handler = async (event) => {
+  const { query } = qs.parse(event.body);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ query }),
+  };
+};
